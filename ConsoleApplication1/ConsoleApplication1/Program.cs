6b8daf68ec8d86a497b507a34f599a65d733b6c8
Linus Berntsson <liberntsson@hotmail.com>
2015-09-24 09:51:09 +0200

    herpa derpa

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("denna labb tar så sjukt onödigt mkt tid");
+                Console.ReadKey();
+            }
+        }
+    }
+}
